9cb1b1fa9d11e6a743d54548495b85652914d6ea
Ivan Sorokin <vanyacpp@gmail.com>
2022-01-07 17:07:01 +0300

    apply license to all files

--- main_window.cpp
+++ main_window.cpp
@@ -1,3 +1,7 @@
+// Copyright Ivan Sorokin 2022. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
 #include "main_window.h"
 #include "auto_visibility_controller.h"
 #include "./ui_main_window.h"
